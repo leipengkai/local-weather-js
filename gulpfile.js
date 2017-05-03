39a3750a26256bffdf41b5eb716d7b63bdef8a6b
Alejandro Mesa Suarez <alejandro.mesa1991@gmail.com>
2017-05-03 18:56:16 -0400

    Gulp Ready

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,48 @@
-var gulp = require('gulp'),
-    plumber = require('gulp-plumber');
-var browserSync = require('browser-sync');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var babel = require('gulp-babel');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
 
-gulp.task('browser-sync', function() {
-  browserSync({
-    server: {
-       baseDir: "./"
-    }
-  });
+gulp.task('css', function () {
+    return gulp.src('./css/*.scss')
+                .pipe(sass().on('error', sass.logError))
+                .pipe(autoprefixer({
+                    browsers: ['last 2 versions'],
+                    cascade: false
+                }))
+                .pipe(gulp.dest('./css/')) 
+                .pipe(browserSync.stream());
 });
 
-gulp.task('bs-reload', function () {
-  browserSync.reload();
+gulp.task('js', function() {
+    return gulp.src('./js/*.js')
+        .pipe(plumber({
+            errorHandler: function(error) {
+                console.log(error.message);
+                this.emit('end');
+            }
+        }))
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest('./js/dist/'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['browser-sync'], function(){
-  gulp.watch("*.html", ['bs-reload']);
+gulp.task('watch', function() {
+    gulp.watch('./css/*.scss', ['css']);
+    gulp.watch('./js/*.js', ['js']);
 });
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['css','watch','serve','js']);
--- js/dist/main.js
+++ js/dist/main.js
@@ -1,19 +1,19 @@
+'use strict';
+
 //jQuery Document Ready
-$(document).ready(function() {
+$(document).ready(function () {
 
     //Autocomplete using Google Maps Api
-    const input = document.getElementById('form_city');
-    let options = {
+    var input = document.getElementById('form_city');
+    var options = {
         types: ['(cities)']
     };
 
     autocomplete = new google.maps.places.Autocomplete(input, options);
 
-
-
     //Function to get icon
     function getIcon(dayornight, getid) {
-        let finalicon;
+        var finalicon = void 0;
 
         if (dayornight == "d") {
             extension = ".png";
@@ -22,7 +22,6 @@ $(document).ready(function() {
         } else {
             extension = ".png";
         }
-
 
         //Thunderstorm
         if (getid == 200 || getid == 211 || getid == 230) {
@@ -34,68 +33,66 @@ $(document).ready(function() {
         }
         //Drizzle
         else if (getid >= 300 && getid <= 321) {
-            finalicon = "light_rain.png";
-        }
-        //Rain
-        else if (getid == 500 || getid == 520) {
-            finalicon = "shower1" + extension;
-        } else if (getid == 521 || getid == 501) {
-            finalicon = "shower2" + extension;
-        } else if (getid == 504 || getid == 522 || getid == 531 || getid == 503) {
-            finalicon = "shower3.png";
-        } else if (getid == 511) {
-            finalicon = "hail.png";
-        }
-        //Snow
-        else if (getid >= 611 && getid <= 620) {
-            finalicon = "sleet.png";
-        } else if (getid == 600) {
-            finalicon = "snow1" + extension;
-        } else if (getid == 601) {
-            finalicon = "snow2" + extension;
-        } else if (getid == 602) {
-            finalicon = "snow3" + extension;
-        } else if (getid == 621) {
-            finalicon = "snow4.png";
-        } else if (getid == 622) {
-            finalicon = "snow5.png";
-        }
-        //Atmosphere
-        else if (getid >= 701 && getid <= 781) {
-            finalicon = "fog" + extension;
-        }
-        //Clear
-        else if (getid == 800) {
-            finalicon = "sunny" + extension;
-        }
-        //Clouds
-        else if (getid == 801) {
-            finalicon = "cloudy1" + extension;
-        } else if (getid == 802) {
-            finalicon = "cloudy2" + extension;
-        } else if (getid == 803) {
-            finalicon = "cloudy3" + extension;
-        } else if (getid == 804) {
-            finalicon = "cloudy4" + extension;
-        }
-        //Extreme
-        else if (getid >= 900 && getid <= 905) {
-            finalicon = "dunno" + extension;
-        } else if (getid == 906 || getid == 511) {
-            finalicon = "hail.png";
-        }
-        //Additional
-        else if (getid >= 951 && getid <= 962) {
-            finalicon = "dunno" + extension;
-        }
+                finalicon = "light_rain.png";
+            }
+            //Rain
+            else if (getid == 500 || getid == 520) {
+                    finalicon = "shower1" + extension;
+                } else if (getid == 521 || getid == 501) {
+                    finalicon = "shower2" + extension;
+                } else if (getid == 504 || getid == 522 || getid == 531 || getid == 503) {
+                    finalicon = "shower3.png";
+                } else if (getid == 511) {
+                    finalicon = "hail.png";
+                }
+                //Snow
+                else if (getid >= 611 && getid <= 620) {
+                        finalicon = "sleet.png";
+                    } else if (getid == 600) {
+                        finalicon = "snow1" + extension;
+                    } else if (getid == 601) {
+                        finalicon = "snow2" + extension;
+                    } else if (getid == 602) {
+                        finalicon = "snow3" + extension;
+                    } else if (getid == 621) {
+                        finalicon = "snow4.png";
+                    } else if (getid == 622) {
+                        finalicon = "snow5.png";
+                    }
+                    //Atmosphere
+                    else if (getid >= 701 && getid <= 781) {
+                            finalicon = "fog" + extension;
+                        }
+                        //Clear
+                        else if (getid == 800) {
+                                finalicon = "sunny" + extension;
+                            }
+                            //Clouds
+                            else if (getid == 801) {
+                                    finalicon = "cloudy1" + extension;
+                                } else if (getid == 802) {
+                                    finalicon = "cloudy2" + extension;
+                                } else if (getid == 803) {
+                                    finalicon = "cloudy3" + extension;
+                                } else if (getid == 804) {
+                                    finalicon = "cloudy4" + extension;
+                                }
+                                //Extreme
+                                else if (getid >= 900 && getid <= 905) {
+                                        finalicon = "dunno" + extension;
+                                    } else if (getid == 906 || getid == 511) {
+                                        finalicon = "hail.png";
+                                    }
+                                    //Additional
+                                    else if (getid >= 951 && getid <= 962) {
+                                            finalicon = "dunno" + extension;
+                                        }
 
         return finalicon;
-
-
     }
 
     //Clicking for more info
-    $("#more").click(function() {
+    $("#more").click(function () {
         $(".div_more").hide();
         $(".search").show().addClass("animated bounceInRight");
     });
@@ -104,42 +101,36 @@ $(document).ready(function() {
     $(".search").hide();
     $(".newsearch").hide();
 
-
     //Animations
-    $(".social a, button").hover(
-        function() {
-            $(this).addClass("animated pulse");
-        },
-        function() {
-            $(this).removeClass("animated pulse");
-        }
-    );
+    $(".social a, button").hover(function () {
+        $(this).addClass("animated pulse");
+    }, function () {
+        $(this).removeClass("animated pulse");
+    });
 
     //Initialize tooltips
-    $(function() {
-        $('[data-toggle="tooltip"]').tooltip()
-    })
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip();
+    });
 
     //Enter key function when enter
-    $("#form_city").keyup(function(event) {
+    $("#form_city").keyup(function (event) {
         if (event.keyCode == 13) {
             $("#startsearch").click();
             $(".newsearch").show().addClass("animated bounceInLeft");
-
         }
     });
-
 
     //API Functionality
     //API KEY: ec83fdc708f434c54152ef547f1a4f8b
     //GEOLOCATION
     var location = "http://ipinfo.io";
-    
+
     var lat, long;
     var geolocation, city, region;
 
     //Pull your location
-    $.getJSON(location, function(data1) {
+    $.getJSON(location, function (data1) {
 
         geolocation = data1.loc.split(',');
         var city = data1.city;
@@ -147,7 +138,6 @@ $(document).ready(function() {
         var lat = geolocation[0];
         var long = geolocation[1];
         var country = data1.country.toLowerCase();
-
 
         $("#latitude").html(lat + " , " + long);
         $("#longitude").html(long);
@@ -163,11 +153,7 @@ $(document).ready(function() {
         //WEATHER
         var api = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=ec83fdc708f434c54152ef547f1a4f8b";
 
-
-
-
-
-        $.getJSON(api, function(data2) {
+        $.getJSON(api, function (data2) {
 
             //Celsius
             var cTemp = (data2.main.temp - 273).toFixed(0);
@@ -179,7 +165,7 @@ $(document).ready(function() {
 
             $("#temp").html(cTemp + " C°");
 
-            $("#temp").click(function() {
+            $("#temp").click(function () {
 
                 if (toggletemp) {
                     $("#temp").html(fTemp + " F°");
@@ -190,33 +176,25 @@ $(document).ready(function() {
                     $("#temp").html(cTemp + " C°");
                     toggletemp = true;
                     $("#changetemp").html("Click to change to F°");
-
                 }
-
             });
 
             var final_icon = getIcon(data2.weather[0].icon.charAt(2), data2.weather[0].id);
             $("#firsticon").attr("src", "images/weather/" + final_icon);
             $("#pressure").html(pressure + " hPa");
             $("#humidity").html(humidity + "%");
-
         });
-
-    },"jsonp");
-
-
+    }, "jsonp");
 
     //SEARCH FUNCTIONALITY
-    $("#startsearch").click(function() {
+    $("#startsearch").click(function () {
 
         $(".newsearch").show().addClass("animated bounceInLeft");
 
         var searchvalue = $("input:text").val();
         var api2 = "http://api.openweathermap.org/data/2.5/weather?q=" + searchvalue + "&appid=ec83fdc708f434c54152ef547f1a4f8b";
 
-
-
-        $.getJSON(api2, function(data3) {
+        $.getJSON(api2, function (data3) {
 
             var yourcity = data3.name;
             var yourcountry = data3.sys.country.toLowerCase();
@@ -228,19 +206,13 @@ $(document).ready(function() {
             var dayornight = data3.weather[0].icon.charAt(2);
             var finalicon = getIcon(dayornight, getid);
 
-
-
-
             $("#anothercountry").attr('src', extraflag);
             $("#yourcity").html(yourcity + " , " + yourcountry.toUpperCase());
             $("#yourtemp").html(yourtemp + " C°");
 
             var toggletemp2 = true;
 
-
-
-
-            $("#yourtemp").click(function() {
+            $("#yourtemp").click(function () {
 
                 if (toggletemp2) {
                     $("#yourtemp").html((yourtemp * 9 / 5 + 32).toFixed(0) + " F°");
@@ -249,21 +221,12 @@ $(document).ready(function() {
                     $("#yourtemp").html(yourtemp + " C°");
                     toggletemp2 = true;
                 }
-
             });
-
 
             $("#yourforecast").html(yourforecast);
             $("#yourpressure").html(data3.main.pressure + " hPa");
             $("#yourhumidity").html(data3.main.humidity + "%");
             $("#newicon").attr("src", "images/weather/" + finalicon);
-
         });
-
     });
-
-
-
-
-
 });
