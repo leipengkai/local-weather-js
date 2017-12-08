e302b713f502c0575ce59e396b05d4ba0ef12437
Alejandro Mesa <alejandro.mesa1991@gmail.com>
2017-12-08 11:57:07 -0500

    Cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,64 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var plumber = require('gulp-plumber');
-var babel = require('gulp-babel');
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync').create();
-var surge = require('gulp-surge')
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var babel = require("gulp-babel");
+var autoprefixer = require("gulp-autoprefixer");
+var browserSync = require("browser-sync").create();
+var surge = require("gulp-surge");
 
-gulp.task('css', function () {
-    return gulp.src('./css/*.scss')
-                .pipe(sass().on('error', sass.logError))
-                .pipe(autoprefixer({
-                    browsers: ['last 2 versions'],
-                    cascade: false
-                }))
-                .pipe(gulp.dest('./css/')) 
-                .pipe(browserSync.stream());
+gulp.task("css", function() {
+  return gulp
+    .src("./css/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest("./css/"))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('js', function() {
-    return gulp.src('./js/*.js')
-        .pipe(plumber({
-            errorHandler: function(error) {
-                console.log(error.message);
-                this.emit('end');
-            }
-        }))
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(gulp.dest('./js/dist/'))
-        .pipe(browserSync.stream());
-});
-
-gulp.task('watch', function() {
-    gulp.watch('./css/*.scss', ['css']);
-    gulp.watch('./js/*.js', ['js']);
-});
-
-gulp.task('serve', function() {
-    browserSync.init({
-        server: {
-            baseDir: './'
+gulp.task("js", function() {
+  return gulp
+    .src("./js/*.js")
+    .pipe(
+      plumber({
+        errorHandler: function(error) {
+          console.log(error.message);
+          this.emit("end");
         }
-    });
-    gulp.watch('*.html').on('change', browserSync.reload);
+      })
+    )
+    .pipe(
+      babel({
+        presets: ["es2015"]
+      })
+    )
+    .pipe(gulp.dest("./js/dist/"))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('deploy',function () {
+gulp.task("watch", function() {
+  gulp.watch("./css/*.scss", ["css"]);
+  gulp.watch("./js/*.js", ["js"]);
+});
+
+gulp.task("serve", function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  gulp.watch("*.html").on("change", browserSync.reload);
+});
+
+gulp.task("deploy", function() {
   return surge({
-    project: '.',         // Path to your static build directory
-    domain: 'weather-javascript.surge.sh'  // Your domain or Surge subdomain
+    project: ".", // Path to your static build directory
+    domain: "weather-javascript.surge.sh" // Your domain or Surge subdomain
   });
 });
 
-gulp.task('default', ['css','watch','serve','js']);
+gulp.task("default", ["css", "watch", "serve", "js"]);
